c7c9bdfdf38772093e5e67778a9b672c91a6a3c8
gyurious <gyurious.coding@gmail.com>
2020-05-21 13:23:34 +0900

    check heroku connect to git

--- server/main.go
+++ server/main.go
@@ -7,7 +7,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Hi hi",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
